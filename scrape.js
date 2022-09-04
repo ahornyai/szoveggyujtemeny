02cce8e51d1e23c04f29b7bacab3b9d1e2981244
Hornyai Alex <ahornyai@gmail.com>
2022-09-04 15:25:57 +0200

    nkp api

--- scrape.js
+++ scrape.js
@@ -1,11 +1,35 @@
 #! /usr/bin/env node
 
+import axios from 'axios';
+
+class NKP {
+  BASE = "https://www.nkp.hu/api";
+  BOOK_STRUCTURE = "/get_book_structure?book_uri_segment=";
+  LESSON_CONTENT = "/get_book_lesson_content?id=";
+  
+  async getLessons(book_uri_segment) {
+    return (await axios.get(this.BASE + this.BOOK_STRUCTURE + book_uri_segment)).data.lessons;
+  }
+
+  async getLessonContent(id) {
+    return (await axios.get(this.BASE + this.LESSON_CONTENT + id)).data.renderedSections;
+  }
+
+}
+
 const config = {
-  "sources": [ //todo: more
-    "https://www.nkp.hu/tankonyv/irodalom_9_szoveggyujtemeny_nat2020/",
+  "book_uris": [ //todo: more
+    "irodalom_9_szoveggyujtemeny_nat2020",
   ]
 }
 
-config.sources.forEach((source) => {
-  console.log("[+] Scraping: " + source);
+const nkp = new NKP();
+
+config.book_uris.forEach(async (source) => {
+  console.log("[+] Downloading: " + source);
+
+  const lessons = await nkp.getLessons(source);
+  const contents = await Promise.all(lessons.map(async (lesson) => await nkp.getLessonContent(lesson.lesson_id), []));
+
+  console.log(contents);
 });
